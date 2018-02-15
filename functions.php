d2ba4457e0e358ce7a83940aa7a27838af9a903f
Elena Melnik <melen76@gmail.com>
2018-02-15 10:20:20 +0200

    Функции вынесены в отдельный файл, добавлена функция-шаблонизатор

--- functions.php
+++ functions.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Elena
- * Date: 13.02.2018
- * Time: 3:54
- */
+
+// Функция подсчета задач в массиве по переданному аргументу - имени категории
+function getCategoryTasksQuantity($tasks_array, $category_name) {
+
+    $quantity = 0;
+
+    // Итерация по массиву, посчитать сколько его элементов относятся к проекту из аргумента $category_name.
+    foreach ($tasks_array as $array_item) {
+        if ($array_item['category'] == $category_name) {
+            $quantity++;
+        }
+    }
+
+    // Если аргумент $category_name равен строке "Все", то вернуть общее число задач в массиве.
+    if ($category_name == 'Все') {
+        $quantity = count($tasks_array);
+    }
+
+    return $quantity;
+}
+
+// Функция фильтрации данных
+function filterText($string) {
+    $html_text = htmlspecialchars($string);
+
+    return $html_text;
+}
+
+// Функция-шаблонизатор
+function includeTemplate($template_file, $template_data_array) {
+    ob_start();
+    extract($template_data_array);
+
+    if (file_exists($template_file)) {
+        $file_content = require_once($template_file);
+    } else {
+        $file_content = '';
+    }
+
+    $file_content = ob_get_clean();
+    return $file_content;
+}
